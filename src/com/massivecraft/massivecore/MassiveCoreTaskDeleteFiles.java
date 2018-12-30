fdb26254b1005b3b6e4a902ab74d1c1cce551b9b
Magnus Ulf <magnus@muj.dk>
2018-12-30 23:42:20 +0100

    This is 3.0.1

--- src/com/massivecraft/massivecore/MassiveCoreTaskDeleteFiles.java
+++ src/com/massivecraft/massivecore/MassiveCoreTaskDeleteFiles.java
@@ -21,7 +21,7 @@ public class MassiveCoreTaskDeleteFiles implements Runnable
 		// Some people got null pointers
 		if (MassiveCoreMConf.get() == null) throw new NullPointerException("mconf");
 		if (MassiveCoreMConf.get().deleteFiles == null) throw new NullPointerException("deleteFiles");
-		
+
 		for (String deleteFile : MassiveCoreMConf.get().deleteFiles)
 		{
 			File file = new File(deleteFile);
